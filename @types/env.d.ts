6a458e1318df43ff93950b71d4fdb7c99e44597a
Danny Fuhriman <danny.fuhriman@okta.com>
2025-07-17 14:15:06 -0700

    add env types + env.sample

---
+++ @types/env.d.ts
@@ -0,0 +1,49 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    /**
+     * If configured use your custom domain.
+     *
+     * @readonly
+     */
+    readonly AUTH0_DOMAIN: string;
+    /**
+     * If using a custom domain, this is a required variable.
+     *
+     * @readonly
+     */
+    readonly AUTH0_MANAGEMENT_DOMAIN?: string;
+    readonly AUTH0_CLIENT_ID: string;
+    /**
+     * Set if a different CLIENT_ID should be used for management API calls.
+     *
+     * @default `AUTH0_CLIENT_ID`
+     * @readonly
+     */
+    readonly AUTH0_MANAGEMENT_CLIENT_ID?: string;
+    readonly AUTH0_CLIENT_SECRET: string;
+    /**
+     * Set if a different CLIENT_SECRET should be used for management API calls.
+     *
+     * @default `AUTH0_CLIENT_SECRET`
+     * @readonly
+     */
+    readonly AUTH0_MANAGEMENT_CLIENT_SECRET?: string;
+    /**
+     * Create a key to encrypt session and transaction cookies.
+     *
+     * Use the following command (or any you desire):
+     * `openssl rand -hex 32`
+     *
+     * @example `bdbf9d6f995a6c2bdf50c5f769f7e3a125c6b6f08eac07097f007adbb6dd140b`
+     * @readonly
+     */
+    readonly AUTH0_SECRET: string;
+    /**
+     * Your application's base URL.
+     *
+     * @example `http://localhost:3000`
+     * @readonly
+     */
+    readonly APP_BASE_URL: string;
+  }
+}
